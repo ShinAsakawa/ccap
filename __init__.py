ae5ce92686e4caf80531442d08cf53fcf3eb0cfd
Shin Asakawa <10644967+ShinAsakawa@users.noreply.github.com>
2023-10-30 10:52:56 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ from .ccap_normalize_neologd import normalize_neologd
 from . import onomatope
 #from . import minnichi
 #from . import vdj2500
-from . import mecab_settings
+#from . import mecab_settings
 #from . import tlpa_o2p # this name seems to be obsolute
 from . import multi_modal
 
